a54c062ddb04591a2fc2fea7a84b54e3e50e0695
Emik123dsa <pengrobinson@mail.ru>
2020-02-18 02:51:42 +0500

    the latest update

--- src/Database.php
+++ src/Database.php
@@ -13,18 +13,19 @@ class Database {
     private $params = [];
     
    
-    private function __construct( array $settings) 
+    public function __construct( array $settings) 
     {
 
         $this->settings = $settings;
 
         $this->connect();
+       
     }
 
     public function connect() 
     {
         
-        $dsn = $this->settings['driver']. ":dbname". $this->settings['db_name'] .";host=". $this->settings['host'];
+        $dsn = $this->settings['driver']. ":host=". $this->settings['host'] .";dbname=". $this->settings['db_name'];
     try{ 
         $this->pdo = new PDO($dsn, $this->settings['db_user'], $this->settings['db_password'], 
         [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->settings['charset']]); 
@@ -41,8 +42,8 @@ class Database {
 
     public function close() 
     {
-        $this->isConnected = false; 
-        $this->pdo = NULL;
+       
+        $this->pdo = null;
     }
 
     private function init(string $query, array $params = []) 
@@ -55,40 +56,62 @@ class Database {
             $this->statement = $this->pdo->prepare($query);
 
             $this->bind($params);
+            
+            if (!empty($this->params)) {
+                foreach($this->params as $param => $key) {
+                    if (is_int($key[1])) {
+                        $type = PDO::PARAM_INT; 
+                    } elseif (is_bool($key[1])) {
+                        $type = PDO::PARAM_BOOL;
+                    } elseif (is_null($key[1])) {
+                        $type = PDO::PARAM_NULL;
+                    } else {
+                        $type = PDO::PARAM_STR;
+                    }
+
+                    $this->statement->bindValue($key[0], $key[1], $type);
+
+                }
+            }
+            $this->statement->execute();
         } catch (PDOException $e) {
             die($e->getMessage());
         }
+        
     }
 
     private function bind(array $params) : void 
     {
-        if (!empty($params) && is_array($params)) {
+        if (!empty($params) and is_array($params)) {
             $columns = array_keys($params);
-
-            foreach ($columns as $column => &$feature) {
+            
+            foreach ($columns as $i => &$column) {
                 $this->params[sizeof($this->params)] = [
-                    ":" .$feature, 
-                    $params[$feature]
+                    ":" .$column, 
+                    $params[$column]
                 ];
-
             }
-        
+            
         }
     }
 
     public function query(string $query, array $params = [], $mode = PDO::FETCH_ASSOC)
     {
         $query = trim(str_replace('\r', ' ', $query));
-        
+       
         $this->init($query, $params); 
-
+        
         $rowStatement = explode(" ", preg_replace("/\s+|\t+|\n+/", " ", $query));
-
+        
+        
         $statement = strtolower($rowStatement[0]); 
-
+        
+        
         if ($statement == 'select' || $statement == 'show') {
+            
             return $this->statement->fetchAll($mode);
         } elseif ($statement == 'insert' || $statement == 'update'|| $statement == 'delete') {
+            
             return $this->statement->rowCount();
         } else {
             return null;
